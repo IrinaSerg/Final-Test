e95241900aa17a43fe69379b01a6416fa90c7880
Irina Sergeeva <Irinka197308@mail.ru>
2022-10-04 19:25:05 +0300

    add logic

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿string[] Filter(string[] strs)
+{
+   string[] result = new string[strs.Length];
+   for(int i = 0; i < strs.Length; i++)
+   {
+        if(strs[i].Length <= 3)
+        {
+            result[i] = strs[i];
+        }
+   } 
+   return result;
+}
+
+void PrintArray(string[] strs)
+{
+    Console.Write("[");
+    for(int i = 0; i < strs.Length; i++)
+    {
+        Console.Write($"{strs[i]}, ");
+    }
+    Console.WriteLine("]"); 
+}
+
+Console.WriteLine("Введите строки, отделяя их запятыми без пробелов:");
+string[] input = Console.ReadLine().Split(",");
+string[] result = Filter(input);
+PrintArray(result);
